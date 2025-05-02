5ff345a3dd9af5bf879080781add89e573b2db27
Kate-Gornostaeva <itsmekate153@gmail.com>
2025-05-02 19:54:53 +1000

    deleted some imports

--- school.py
+++ school.py
@@ -1,12 +1,9 @@
 import asyncio
 import sqlite3
-from aiogram import Bot, Dispatcher, types, F
+from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command, CommandStart
-from aiogram.types import FSInputFile
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.fsm.storage.memory import MemoryStorage
-import aiohttp
 import logging
 from conf import TOKEN
 
