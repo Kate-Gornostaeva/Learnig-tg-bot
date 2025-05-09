f8f362756c87d55586d1096ef8799379273df678
Kate-Gornostaeva <itsmekate153@gmail.com>
2025-05-09 11:09:28 +1000

    added cats and nasa bots

---
+++ cats.py
@@ -0,0 +1,57 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message
+from conf import TOKEN, CATS_API_KEY
+import requests
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+def get_cats_breeds():
+    url = 'https://api.thecatapi.com/v1/breeds'
+    headers = {"x-api-key": CATS_API_KEY}
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+def get_cat_image_by_breed (breed_id):
+    url = f'https://api.thecatapi.com/v1/images/search?breed_ids={breed_id}'
+    headers = {"x-api-key": CATS_API_KEY}
+    response = requests.get(url, headers=headers)
+    data =response.json()
+    return data[0]['url']
+
+def get_breed_info(breed_name):
+    breeds = get_cats_breeds()
+    for breed in breeds:
+        if breed['name'].lower() == breed_name.lower():
+            return breed
+    return None
+
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer("Привет! Я бот для поиска котов по породам. Пожалуйста, введите название породы кота.")
+
+
+@dp.message()
+async def send_cat_info (message: Message):
+    breed_name = message.text
+    breed_info = get_breed_info(breed_name)
+    if breed_info:
+        breed_id = breed_info['id']
+        cat_image_url = get_cat_image_by_breed(breed_id)
+        info  = (f"Название породы: {breed_info['name']}\n"
+                 f"Описание: {breed_info['description']}\n"
+                 f"Продолжительность жизни: {breed_info['life_span'] }\n")
+        await message.answer_photo(cat_image_url, caption=info)
+    else:
+        await message.answer("Порода кота не найдена.")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ nasa.py
@@ -0,0 +1,37 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message
+from conf import TOKEN, NASA_API_KEY
+import requests
+import random
+import datetime
+from datetime import datetime, timedelta
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+def get_random_apod():
+   end_date = datetime.now()
+   start_date = end_date - timedelta(days=365)
+   random_date = start_date + (end_date - start_date) * random.random()
+   formatted_date = random_date.strftime('%Y-%m-%d')
+   url = f'https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}&date={formatted_date}'
+   response = requests.get(url)
+   return response.json()
+
+@dp.message(Command("apod"))
+async def send_apod(message: types.Message):
+    apod = get_random_apod()
+    photo_url = apod['url']
+    title = apod['title']
+    explanation = apod['explanation']
+    await message.answer_photo(photo_url, caption=f"{title}\n\n{explanation}")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
